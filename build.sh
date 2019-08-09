9bf508b927d61cc73fb67fe8aa968a562cf4a124
Votre Nom <jeremiah252@hotmail.com>
2019-08-09 02:01:11 +0200

    Revert back

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PROJECT=RPi DEVICE=RPi4 ARCH=arm make image
-PROJECT=RPi DEVICE=RPi2 ARCH=arm make image
-PROJECT=RPi DEVICE=RPi3 ARCH=arm make image
-PROJECT=Rockchip DEVICE=RK3399 UBOOT_SYSTEM=rockpro64 ARCH=arm make image
