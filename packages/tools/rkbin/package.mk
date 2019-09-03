c52c1293414aaa36432b8bd92ba617b6e3fc6f22
Oleg Ivanov <balbes-150@yandex.ru>
2019-09-03 10:33:52 +0300

    C1 fix u-boot RK

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="rkbin"
-# Version is: Kwiboo/tag:libreelec-ba436b9
-PKG_VERSION="ba436b9d616318a9437895457f6bbef1cc873e2b"
-PKG_SHA256="72c2ef6ec1fe79da7c701056662343b9e1df1cf20e5df10bafc4ec0619ef4578"
+# Version is: Kwiboo/tag:libreelec-bd67404
+PKG_VERSION="bd67404b0cd9019439ce663edf444b5252066288"
+PKG_SHA256="eedf7ba34cd25f6ca642ace5f8759c6ea6ebf1ce63843c236cfbf1d01de115a9"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
